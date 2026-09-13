df5cf0b96f6ad306948c301558b4cccac0e3e442
rts <lts06069@naver.com>
2026-09-13 09:02:18 +0900

    fix: Godot 실행 경로 수정

--- run-general-editor.bat
+++ run-general-editor.bat
@@ -1,5 +1,5 @@
 @echo off
-set "GODOT=D:\LOCAL-WORK-STATION\Godot_v4.7.2-stable_mono_win64\Godot_v4.7.2-stable_mono_win64.exe"
+set "GODOT=D:\LOCAL-WORK-STATION\Godot_v4.7.2-stable_win64\Godot_v4.7.2-stable_mono_win64.exe"
 if not exist "%GODOT%" (
   echo Godot executable not found: "%GODOT%"
   exit /b 1
--- run.bat
+++ run.bat
@@ -1,4 +1,4 @@
 @echo off
 rem Run SanguoSLG (Godot .NET build required)
-set "GODOT=D:\LOCAL-WORK-STATION\Godot_v4.7.2-stable_mono_win64\Godot_v4.7.2-stable_mono_win64.exe"
+set "GODOT=D:\LOCAL-WORK-STATION\Godot_v4.7.2-stable_win64\Godot_v4.7.2-stable_mono_win64.exe"
 "%GODOT%" --path "%~dp0SanguoSLG.Game"
